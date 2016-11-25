af2c2593fb9c65c4b57906b53ecebb3cecf59f7d
zhaichen <zhaichen@10.100.100.9>
2016-11-25 10:40:10 +0800

    util类重构

--- analysis.center/src/main/java/com/zc/util/PropertyFileReadUtil.java
+++ analysis.center/src/main/java/com/zc/util/PropertyFileReadUtil.java
@@ -1,6 +1,5 @@
 package com.zc.util;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -27,24 +26,20 @@ public class PropertyFileReadUtil {
 	public static Map<String, String> getAddressMap() {
 		if (addressMap == null) {
 			addressMap = new HashMap<String, String>();
-			Properties properties = initPropertiesByFile("properties/address.properties");
-			Iterator<String> iterator = properties.stringPropertyNames().iterator();
-
-			while (iterator.hasNext()) {
-				String key = iterator.next();
-				addressMap.put(key, properties.getProperty(key));
-			}
+			Properties properties = loadFileInProperties("properties/address.properties");
+			
+			initMapByPropeties(properties, addressMap);
 		}
 
 		return addressMap;
 	}
 
 	/**
-	 * 公用方法，根据文件名获取初始化的Properties类
+	 * 公用方法，将指定文件的内容读取到Properties类中
 	 * @param fileName
 	 * @return
 	 */
-	private static Properties initPropertiesByFile(String fileName) {
+	private static Properties loadFileInProperties(String fileName) {
 		Properties properties = new Properties();
 		try {
 			InputStream inputStream = PropertyFileReadUtil.class.getClassLoader().getResourceAsStream(fileName);
@@ -57,4 +52,19 @@ public class PropertyFileReadUtil {
 
 		return properties;
 	}
+	
+	/**
+	 * 将propeties中的内容放到targetMap中
+	 * @param properties
+	 * @param targetMap
+	 */
+	private static void initMapByPropeties(Properties properties, Map<String, String> targetMap){
+		Iterator<String> iterator = properties.stringPropertyNames().iterator();
+
+		while (iterator.hasNext()) {
+			String key = iterator.next();
+			targetMap.put(key, properties.getProperty(key));
+		}
+	}
+	
 }
